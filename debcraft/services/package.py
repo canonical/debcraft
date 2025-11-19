e9a0f7d4bc79d7eac4da9092739ea77c5fc30453
Claudio Matsuoka <claudio.matsuoka@canonical.com>
2025-11-19 12:53:09 -0300

    feat: initial creation of deb package
    Create control and data tarballs and archive them in a .deb file,
    currently based on the main project name. This will be adjusted
    to use the correct package name after the project model is in
    place. Control metadata and checksum files will be added in a
    separate PR.
    
    Signed-off-by: Claudio Matsuoka <claudio.matsuoka@canonical.com>

--- debcraft/services/package.py
+++ debcraft/services/package.py
@@ -18,13 +18,22 @@
 
 from __future__ import annotations
 
+import os
 import pathlib
+import subprocess
 import tarfile
+import tempfile
+import textwrap
 from typing import cast
 
+import craft_application
+import zstandard as zstd
 from craft_application import services
+from craft_platforms import BuildInfo
 
 from debcraft import models
+
+_ZSTD_COMPRESSION_LEVEL = 3
 
 
 class Package(services.PackageService):
@@ -36,16 +45,37 @@ class Package(services.PackageService):
         :param dest: Directory into which to write the package(s).
         :returns: A list of paths to created packages.
         """
-        self.write_metadata(prime_dir)
         project = self._services.get("project").get()
         build_plan = self._services.get("build_plan").plan()[0]
 
-        binary_package_name = (
-            f"{project.name}_{project.version}_{build_plan.platform}.tar.xz"
-        )
-        with tarfile.open(dest / binary_package_name, mode="w:xz") as tar:
-            tar.add(prime_dir, arcname=".", recursive=True)
-        return [dest / binary_package_name]
+        with tempfile.TemporaryDirectory() as tmpdir:
+            cwd = pathlib.Path().absolute()
+            deb_name = (
+                dest.absolute()
+                / f"{project.name}_{project.version}_{build_plan.platform}.deb"
+            )
+
+            try:
+                os.chdir(tmpdir)
+                _create_data_file(pathlib.Path(tmpdir), prime_dir)
+                _create_control_file(pathlib.Path(tmpdir), project, build_plan)
+
+                pathlib.Path("debian-binary").write_text("2.0\n")
+                subprocess.run(
+                    [
+                        "ar",
+                        "rcs",
+                        deb_name,
+                        "debian-binary",
+                        "control.tar.zstd",
+                        "data.tar.zstd",
+                    ],
+                    check=True,
+                )
+            finally:
+                os.chdir(cwd)
+
+        return [deb_name]
 
     @property
     def metadata(self) -> models.Metadata:
@@ -59,3 +89,49 @@ class Package(services.PackageService):
             base=project.base,
             architecture=build_plan.build_for,
         )
+
+
+def _create_data_file(path: pathlib.Path, prime_dir: pathlib.Path) -> None:
+    """Create the data.tar.zstd file containing the prime contents.
+
+    :param path: Directory where the data.tar.zstd file will be created.
+    :param prime_dir: Directory containing the files to package.
+    """
+    # This should return md5sums to use in the control metadata
+    data_path = path / "data.tar.zstd"
+    with data_path.open("wb") as data_zstd:
+        zcomp = zstd.ZstdCompressor(level=_ZSTD_COMPRESSION_LEVEL)
+        with zcomp.stream_writer(data_zstd) as comp:
+            with tarfile.open(fileobj=comp, mode="w") as tar:
+                tar.add(prime_dir.absolute(), arcname=".")
+
+
+def _create_control_file(
+    path: pathlib.Path, project: craft_application.models.Project, build_plan: BuildInfo
+) -> None:
+    """Create the control.tar.zstd file containing package metadata.
+
+    :param path: Directory where the control.tar.zstd file will be created.
+    :param project: The project model.
+    :param build_plan: Platform information.
+    """
+    control_path = path / "control.tar.zstd"
+
+    # Change to use package data from the project model
+    control_data = textwrap.dedent(
+        f"""\
+        Package: {project.name}
+        Version: {project.version}
+        Architecture: {build_plan.platform}
+    """
+    )
+    control = pathlib.Path("control")
+    control.write_text(control_data)
+
+    with control_path.open("wb") as control_zstd:
+        zcomp = zstd.ZstdCompressor(level=_ZSTD_COMPRESSION_LEVEL)
+        with zcomp.stream_writer(control_zstd) as comp:
+            with tarfile.open(fileobj=comp, mode="w") as tar:
+                tar.add("control")
+
+    control.unlink()
