25e69d2df90af2bcfde131beb435647bb14efe09
Alex Lowe <alex.lowe@canonical.com>
2025-11-20 09:22:11 -0500

    fix(test): add a maintainer field

--- tests/integration/debcraft/test_application.py
+++ tests/integration/debcraft/test_application.py
@@ -67,6 +67,7 @@ def test_debcraft_pack_clean(monkeypatch, tmp_path, host_architecture: str):
                 name: test-deb
                 base: ubuntu@24.04
                 version: "1.0"
+                maintainer: Mike Maintainer <maintainer@example.com>
                 platforms:
                     amd64:
 
